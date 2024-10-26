37f8277a8ef1332c3a6de0d576fdea0236c64153
nazim-fs <Nazim.Shirgar@ezypay.com>
2024-10-26 12:03:54 +0530

    changes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,12 +2,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name              = var.eks_cluster_name
-  cluster_version           = var.eks_version
-  subnet_ids                = var.subnet_ids
-  vpc_id                    = var.vpc_id
-  enable_irsa               = true
-  cluster_security_group_id = var.cluster_sg_id
+  cluster_name                   = var.eks_cluster_name
+  cluster_version                = var.eks_version
+  subnet_ids                     = var.subnet_ids
+  vpc_id                         = var.vpc_id
+  enable_irsa                    = true
+  cluster_endpoint_public_access = true
+  cluster_security_group_id      = var.cluster_sg_id
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2_x86_64"
